fe69c0884c5c6b4b7c6419f166333ac163c22243
CHIA-JUI YEH <noname19930930@gmail.com>
2017-04-15 20:47:40 +0800

    init drive module

---
+++ module/Drive.py
@@ -0,0 +1,118 @@
+import os
+import queue
+import subprocess
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+# constant
+PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)) + '/temp/'
+
+# global var
+drive = None
+
+class Object:
+	def __init__(self, var):
+		self.id = var['id']
+		self.title = var['title']
+		self.parents = var['parents']
+
+	def run(self):
+		# download
+		try:
+			self.download()
+		except Exception as e:
+			print(e)
+			print('[%10s] %s' % ('Error', 'Drive.download()'))
+			return
+
+		"""
+		# encrypt
+		if (ENCRYPTION):
+			try:
+				self.encrypt()
+			except:
+				print('[%10s] %s' % ('Error', 'Drive.encrypt()'))
+				return
+		"""
+		
+		# upload
+		try:
+			self.upload()
+		except:
+			print('[%10s] %s' % ('Error', 'Drive.upload()'))
+
+		# remove file
+		#os.remove(PATH + self.title)
+		#os.remove(PATH + self.title + '.zip')
+
+	def download(self):
+		print('[%10s] %s' % ('Download', self.title))
+		file = drive.CreateFile({'id': self.id})
+		file.GetContentFile(PATH + self.title)
+
+	def encrypt(self):
+		print('[%10s] %s' % ('Encrypt', self.title))
+		subprocess.call(['zip', '--password', PATH + self.parents[0]['id'], PATH + self.title + '.zip', PATH + self.title])
+
+	def upload(self):
+		print('[%10s] %s' % ('Upload', self.title))
+		file = drive.CreateFile({"title": self.title, "parents": [{"kind": "drive#fileLink", "id": self.parents[0]['id']}]})
+		file.SetContentFile(PATH + self.title)
+		file.Upload()
+
+# OAuth
+def auth():
+	print('[%10s] %s' % ('Auth', 'Google API'))
+
+	# var
+	global drive
+
+	# OAuth
+	gauth = GoogleAuth()
+	gauth.LoadCredentialsFile('mycreds.txt')
+
+	# load saved failed
+	if gauth.credentials is None:
+		gauth.LocalWebserverAuth()
+	# refresh if expired
+	elif gauth.access_token_expired:
+		try:
+			gauth.Refresh()
+		except:
+			gauth.LocalWebserverAuth()
+	# init new and save
+	else:
+		gauth.Authorize()
+
+	# save current credentials
+	gauth.SaveCredentialsFile('mycreds.txt')
+
+	drive = GoogleDrive(gauth)
+
+def match(source, dst):
+	# var
+	matched = []
+
+	# list files
+	source_files = drive.ListFile({'q': "'%s' in parents and trashed=false" % source}).GetList()
+	dst_files = drive.ListFile({'q': "'%s' in parents and trashed=false" % dst}).GetList()
+	dst_titles = [i['title'] for i in dst_files]
+
+	for i in source_files:
+		# folder
+		if i['mimeType'] == 'application/vnd.google-apps.folder':
+			if i['title'] not in dst_titles:
+				# create folder
+				temp = drive.CreateFile({'title': i['title'], "parents":  [{"id": dst}], "mimeType": "application/vnd.google-apps.folder"})
+				temp.Upload()
+				matched.extend(match(i['id'], temp['id']))
+			else:
+				temp = dst_files[dst_titles.index(i['title'])]
+				matched.extend(match(i['id'], temp['id']))
+		# not folder
+		elif i['title'] not in dst_titles:
+			i['parents'] = [{"kind": "drive#fileLink", "id": dst}]
+			#print(i)
+			matched.append(Object(i))
+
+	return matched
