445024fc5f0e39f7ce09f76f773f86e9b3d286fc
noname0930 <noname19930930@gmail.com>
2017-04-17 17:04:26 +0800

    rm debug message

--- module/Encryption.py
+++ module/Encryption.py
@@ -5,14 +5,11 @@ import subprocess
 PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)) + '/temp/'
 
 def encrypt(file, password):
-	print('[%10s] %s' % ('Encrypt', file))
-	print('ENCODE %s %s' % (file, password))
+	print('[%10s] %s with pw: %s' % ('Encrypt', file, password))
 	subprocess.call(['zip', '-q', '-j', '--password', password, PATH + file + '.zip', PATH + file])
 	os.remove(PATH + file)
 
 def decrypt(file, password):
-	print('[%10s] %s' % ('Decrypt', file))
-	print('DECODE %s %s' % (file, password))
-	subprocess.call(['unzip', '-j', '-P', password, PATH + file])
-	print(' '.join(['unzip', '-j', '-P', password, '-d', PATH[:-1], PATH + file]))
-	#os.remove(PATH + file)
+	print('[%10s] %s with pw: %s' % ('Decrypt', file, password))
+	subprocess.call(['unzip', '-q', '-j', '-P', password, PATH + file])
+	os.remove(PATH + file)
