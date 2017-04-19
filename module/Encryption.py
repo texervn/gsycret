741905c5b4aba80d59ed3d001fa833ac0279c44e
CHIA-JUI YEH <noname19930930@gmail.com>
2017-04-19 23:12:24 +0800

    Fix bug

--- module/Encryption.py
+++ module/Encryption.py
@@ -5,9 +5,9 @@ import subprocess
 from . import Drive
 
 def encrypt(file, password):
-	print('[%10s] %s with pw: %s' % ('Encrypt', file.path + file.title, password))
-	subprocess.call(['zip', '-q', '-j', '--password', password, file.path + file.title + '.zip', file.path + file.title])
+	print('[%10s] %s with pw: %s' % ('Encrypt', file.title, password))
+	subprocess.call(['zip', '-q', '-j', '--password', password, file.source + file.title + '.zip', file.source + file.title])
 	
 def decrypt(file, password):
-	print('[%10s] %s with pw: %s' % ('Decrypt', file.path + file.title, password))
-	subprocess.call(['unzip', '-q', '-j', '-P', password, file.path + file.title])
+	print('[%10s] %s with pw: %s' % ('Decrypt', file.title, password))
+	subprocess.call(['unzip', '-q', '-j', '-P', password, '-d', file.destination, file.destination + file.title])
