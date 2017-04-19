ce58c5875d52032e60e58359a199ca1f1fecc8df
CHIA-JUI YEH <noname19930930@gmail.com>
2017-04-19 20:04:53 +0800

    testing

--- backup.py
+++ backup.py
@@ -3,6 +3,7 @@ import sys
 import time
 import json
 import queue
+import argparse
 import threading
 import subprocess
 from os import listdir
@@ -10,18 +11,36 @@ from os.path import isfile, join
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-# customized module
+# custom module
 from module import Drive
+from module import Thread
 from module import Encryption
 
 # constant
 PATH = os.path.dirname(os.path.abspath(__file__))
 
-# global var
-CASE = None
-ENCRYPTION = None
-THREADS_NUM = None
-q = queue.Queue()
+def parse_argv():
+	# init
+	parser = argparse.ArgumentParser()
+
+	# add argument
+	parser.add_argument('-m', help='choose operation', choices=('push', 'pull', 'merge'))
+	parser.add_argument('-s', action='store', help='source')
+	parser.add_argument('-d', action='store', help='destination')
+	parser.add_argument('-p', action='store', help='password')
+	parser.add_argument('-a', action='store_true', help='auto encryption', default=False)
+	parser.add_argument('-t', action='store', help='threads_num', type=int, default=4)
+	results = parser.parse_args()
+	
+	return {
+		'name': 'Default',
+		'mode': results.m,
+		'source': results.s,
+		'destination': results.d,
+		'password': results.p,
+		'auto': results.a,
+		'threads_num': results.t
+	}
 
 def load_config(file):
 	try:
@@ -32,71 +51,6 @@ def load_config(file):
 		print('[%10s] %s' % ('Error', 'load_config()'))
 		exit()
 
-def merge():
-	while q.qsize() > 0:
-		obj = q.get()
-
-		try:
-			obj.download()
-		except Exception as e:
-			print('[%10s] %s' % ('Error', str(e)))
-
-		if ENCRYPTION:
-			try:
-				Encryption.encrypt(obj.title, obj.destination)
-				obj.title += '.zip'
-				#Encryption.decrypt(obj.title, obj.parents[0]['id'])
-				#obj.title = obj.title[:obj.title.rfind('.')]
-			except:
-				print('[%10s] %s' % ('Error', str(e)))
-
-		try:
-			obj.upload()
-		except Exception as e:
-			print('[%10s] %s' % ('Error', str(e)))
-
-		time.sleep(1)
-
-def push():
-	while q.qsize() > 0:
-		obj = q.get()
-
-		if ENCRYPTION:
-			try:
-				Encryption.encrypt(obj.title, obj.destination)
-				obj.title += '.zip'
-			except:
-				print('[%10s] %s' % ('Error', str(e)))
-
-		try:
-			obj.upload()
-		except Exception as e:
-			print('[%10s] %s' % ('Error', str(e)))
-
-		time.sleep(1)
-
-def next():
-	
-	if CASE == 'merge':
-		merge()
-	elif CASE == 'push':
-		push()
-
-def run():
-	# init
-	threads = []
-	for i in range(0, THREADS_NUM):
-		t = threading.Thread(target=next, name='T' + str(i))
-		threads.append(t)
-
-	# run
-	for i in range(0, len(threads)):
-		threads[i].start()
-
-	# wait until finish
-	while any(thread.is_alive() for thread in threads):
-		time.sleep(1)
-
 if __name__ == '__main__':
 	# var
 	tasks = []
@@ -105,31 +59,32 @@ if __name__ == '__main__':
 
 	# load config
 	config = load_config('config.json')
-	ENCRYPTION, THREADS_NUM = config['encryption'], config['threads_num']
+	Thread.ENCRYPTION, Thread.THREADS_NUM = config['encryption'], config['threads_num']
 
 	# check argv
 	if len(sys.argv) == 1:
 		tasks = load_config('backup.json')	
-	elif len(sys.argv) == 3:
-		tasks = [{
-			"name": "Default",
-			"source": sys.argv[1],
-			"destination": sys.argv[2]
-		}]
 	else:
-		print('[%10s] %s' % ('Usage', '<source> <destination>'))
-		exit()
+		tasks = [parse_argv()]
 
 	# init
 	Drive.auth()
 
 	for task in tasks:
-		# match
-		CASE = 'push'
-		matched = Drive.push(task['source'], task['destination'])
+		# var
+		matched = []
+
+		# init
+		if task['mode'] == 'push':
+			matched = Drive.push(task['source'], task['destination'])
+		elif task['mode'] == 'pull':
+			matched = Drive.pull(task['source'], task['destination'])
+		elif task['mode'] == 'merge':
+			matched = Drive.merge(task['source'], task['destination'])
+		
 		print('[%10s] %d files found' % ('Match', len(matched)))
 
-	# to queue
-	list(map(q.put, matched))
+		# to queue
+		list(map(Thread.q.put, matched))
 
-	run()
+		Thread.run(getattr(Thread.Object, task['mode']))
