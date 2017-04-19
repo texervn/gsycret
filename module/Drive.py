03fe25997b9a0c0666d5dafb1f627e16bf311f93
CHIA-JUI YEH <noname19930930@gmail.com>
2017-04-19 11:26:13 +0800

    testing all functions

--- module/Drive.py
+++ module/Drive.py
@@ -25,16 +25,10 @@ class Object:
 		file.GetContentFile(self.path + self.title)
 
 	def upload(self):
-
-		print(self.title + ' @@@ ' + self.path)
-
 		print('[%10s] %s' % ('Upload', self.title))
 		file = drive.CreateFile({"title": self.title, "parents": [{"kind": "drive#fileLink", "id": self.destination}]})
 		file.SetContentFile(self.path + self.title)
 		file.Upload()
-
-		# remove useless
-		#os.remove(PATH + self.title)
 
 # OAuth
 def auth():
@@ -47,7 +41,7 @@ def auth():
 	gauth = GoogleAuth()
 	gauth.LoadCredentialsFile('mycreds.txt')
 
-	# load saved failed
+	# load failed
 	if gauth.credentials is None:
 		gauth.LocalWebserverAuth()
 	# refresh if expired
@@ -56,7 +50,6 @@ def auth():
 			gauth.Refresh()
 		except:
 			gauth.LocalWebserverAuth()
-	# init new and save
 	else:
 		gauth.Authorize()
 
@@ -65,73 +58,93 @@ def auth():
 
 	drive = GoogleDrive(gauth)
 
-def merge(source, dst):
+def merge(source, destination):
 	# var
 	matched = []
 
 	# ls
-	source_files = drive.ListFile({'q': "'%s' in parents and trashed=false" % source}).GetList()
-	dst_files = drive.ListFile({'q': "'%s' in parents and trashed=false" % dst}).GetList()
-	dst_titles = [i['title'] for i in dst_files]
-
-	for i in source_files:
+	s_files = drive.ListFile({'q': "'%s' in parents and trashed=false" % source}).GetList()
+	d_files = drive.ListFile({'q': "'%s' in parents and trashed=false" % destination}).GetList()
+	
+	for i in s_files:
 		# folder
 		if i['mimeType'] == 'application/vnd.google-apps.folder':
-			if i['title'] not in dst_titles:
-				# create folder
-				temp = drive.CreateFile({'title': i['title'], "parents":  [{"id": dst}], "mimeType": "application/vnd.google-apps.folder"})
-				temp.Upload()
+			if any(j for j in d_files if j['title'] == i):
+				temp = next(j for j in d_files if j['title'] == i)
 				matched.extend(merge(i['id'], temp['id']))
 			else:
-				temp = dst_files[dst_titles.index(i['title'])]
+				# create folder
+				temp = drive.CreateFile({'title': i['title'], "parents":  [{"id": destination}], "mimeType": "application/vnd.google-apps.folder"})
+				temp.Upload()
 				matched.extend(merge(i['id'], temp['id']))
 		# not folder
-		elif i['title'] not in dst_titles:
+		elif not any(j for j in d_files if j['title'] == i):
 			i['path'] = PATH
-			i['destination'] = dst
+			i['destination'] = destination
 			matched.append(Object(i))
 
 	return matched
 
-def push(source, dst):
+def push(source, destination):
 	# var 
 	matched = []
 
 	# ls
-	local = listdir(source)
-	remote = drive.ListFile({'q': "'%s' in parents and trashed=false" % dst}).GetList()
-	remote_title = [i['title'] for i in remote]
+	s_files = listdir(source)
+	d_files = drive.ListFile({'q': "'%s' in parents and trashed=false" % destination}).GetList()
 
-	#print(remote)
-	#print(remote_title)
-	
-	#return []
-
-	for i in local:
+	for i in s_files:
 		# is folder
 		if os.path.isdir(os.path.join(source, i)):		
-			if any(j for j in remote if j['title'] == i):
-				temp = next(j for j in remote if j['title'] == i)
-				matched.extend(push(i, temp['id']))
+			if any(j for j in d_files if j['title'] == i):
+				temp = next(j for j in d_files if j['title'] == i)
+				matched.extend(push(os.path.join(source, i), temp['id']))
 			else:
 				# create folder
-				temp = drive.CreateFile({'title': i, "parents":  [{"id": dst}], "mimeType": "application/vnd.google-apps.folder"})
+				temp = drive.CreateFile({'title': i, "parents":  [{"id": destination}], "mimeType": "application/vnd.google-apps.folder"})
 				temp.Upload()
 				print(os.path.join(source, i))
 				matched.extend(push(os.path.join(source, i), temp['id']))
-
 		# is file
 		else:
-			if i not in remote_title:
+			if not any(j for j in d_files if j['title'] == i):
 				matched.append(Object({
 					"id": "",
 					"path": source + '/',
 					"title": i,
-					"parents": dst,
-					"destination": dst
+					"parents": destination,
+					"destination": destination
 				}))
 
 	return matched
 
-def pull(source, dst):
-	return
+def pull(source, destination):
+	# var
+	matched = []
+
+	# ls
+	s_files = drive.ListFile({'q': "'%s' in parents and trashed=false" % source}).GetList()
+	d_files = listdir(destination)
+	
+	for i in s_files:
+		# folder
+		if i['mimeType'] == 'application/vnd.google-apps.folder':
+			# existed
+			if any(j for j in d_files if j == i['title'] and os.path.isdir(os.path.join(destination, j))):
+				temp = next(j for j in d_files if j == i['title'])
+				matched.extend(pull(i['id'], os.path.join(destination, temp)))
+			# not existed
+			else:
+				os.mkdir(os.path.join(destination, i['title']))
+				matched.extend(pull(i['id'], os.path.join(destination, i['title'])))
+		# not folder
+		elif not any(j for j in d_files if j == i['title'] and not os.path.isdir(os.path.join(destination, j))):
+			matched.append(Object({
+				"id": i['id'],
+				"path": destination + '/',
+				"title": i['title'],
+				"parents": source,
+				"destination": destination
+			}))
+
+	return matched
