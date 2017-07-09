0e082cb6b8f158d2cc004f6606aea8fa16e818e9
noname0930 <noname19930930@gmail.com>
2017-07-10 00:05:39 +0800

    temp

---
+++ gsycret/encryption.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+
+# custom
+from gsycret.drive import Drive
+
+"""
+def encrypt(file, password):
+	print('[%10s] %s with pw: %s' % ('Encrypt', file.title, password))
+	subprocess.call(['zip', '-q', '-j', '--password', password, file.source + file.title + '.zip', file.source + file.title])
+	
+def decrypt(file, password):
+	print('[%10s] %s with pw: %s' % ('Decrypt', file.title, password))
+	subprocess.call(['unzip', '-q', '-j', '-P', password, '-d', file.destination, file.destination + file.title])
+"""
